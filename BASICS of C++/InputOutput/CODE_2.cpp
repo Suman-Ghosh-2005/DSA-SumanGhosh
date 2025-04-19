3ae791c86de32509ff6b80a72f1c44f6116be1b3
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-04-19 19:31:14 +0530

    done

---
+++ BASICS of C++/InputOutput/CODE_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+// to include all the header files you can use #include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout << "Enter the value of x and y" << endl;
+    cin >>x>>y; 
+    cout << "The value of x is :" <<x <<endl << "the value of y isd : "<< y;
+
+
+    return 0;
+}
