21a0c1d0055c487463265984f58414dd08c464d0
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-04-19 20:06:07 +0530

    conditional statements done

---
+++ BASICS of C++/CONDITIONAL statements/code.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int age;
+
+  cin >> age;
+
+  if(age>=18)
+  {
+    cout<< "ADULT !"<< endl;
+  }
+  else
+  {
+    cout<< "NOT ADULT !"<< endl;
+  }
+
+    return 0;
+}
