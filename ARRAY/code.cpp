ad2fea5edb0e6f80251a530c826a1e8671da6769
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-04-19 21:07:04 +0530

    array basics

---
+++ ARRAY/code.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  
+    int array[5];
+
+    cout << " enter the 5 elements of array";
+
+    for(int i=0 ; i<5 ; i++)
+    {
+        cin >> array[i];
+    }
+
+    cout << "The array is" << endl;
+
+    for(int i=0 ; i<5 ; i++)
+    {
+       cout << array[i] << " ";
+    }
+
+
+
+    return 0;
+}
