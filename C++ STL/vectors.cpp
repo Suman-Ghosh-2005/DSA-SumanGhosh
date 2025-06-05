37925083657f33a7dabc70838b2192325befc984
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-05 21:36:31 +0530

    done

--- C++ STL/vectors.cpp
+++ C++ STL/vectors.cpp
@@ -9,6 +9,14 @@ vector<int> v ;
 v.push_back(5);
 
 v.emplace_back(2); // generally emplace_back is faster than push_back.
+
+vector<pair<int,int>> vec;
+vec.push_back({1,2});
+vec.emplace_back(3,4); // emplace_back constructs the pair in place, which can be more efficient.
+
+
+
+
 }
 
 int main()
