1b8a9fed3e8059b5dcae3b7302492b93e911935a
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-05 21:52:50 +0530

    done

--- C++ STL/vectors.cpp
+++ C++ STL/vectors.cpp
@@ -4,17 +4,25 @@ using namespace std;
 
 void ExplainPair()
 {
-vector<int> v ;
+    vector<int> v;
 
-v.push_back(5);
+    v.push_back(5);
 
-v.emplace_back(2); // generally emplace_back is faster than push_back.
+    v.emplace_back(2); // generally emplace_back is faster than push_back.
 
-vector<pair<int,int>> vec;
-vec.push_back({1,2});
-vec.emplace_back(3,4); // emplace_back constructs the pair in place, which can be more efficient.
+    vector<pair<int, int>> vec;
+
+    vec.push_back({1, 2});
+    vec.emplace_back(3, 4); // emplace_back constructs the pair in place, which can be more efficient.
+
+    vector<int> v(5,100); // creates a vector of size 5, each initialized to 100
 
 
+    vector<int> v(5); // creates a vector of size 5, each initialized to 0
+
+    vector<int> v1(5,10);
+
+    vector<int> v2(v1); // creates a copy of v1
 
 
 }
