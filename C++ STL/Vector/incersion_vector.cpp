e201976386ecb06c10b03dd21cc6c62848ecb979
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-06 13:32:47 +0530

    done

---
+++ C++ STL/Vector/incersion_vector.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void ExplainPair()
+{
+    vector<int> v= {1, 2, 3, 4, 5};
+    vector<int> v1= {10, 20, 30, 48, 57};
+    v.insert(v.begin()+3,80); // inserts 80 at index 3
+    // for(int i=0;i<v.size();i++)
+    // {
+    //     cout << v[i] << " "; // prints the elements of the vector
+    // }
+
+
+    v.insert(v.begin()+1, 3 ,5); // inserts 3 copies of 5 at index 1
+    
+   vector<int>copy(2,5);
+   v.insert(v.begin(),copy.begin(),copy.end());
+
+   v.pop_back(); // removes the last element from the vector
+   
+   v.swap(v1); // swaps the contents of v and v1
+   v.clear(); // clears the vector, making it empty
+
+   cout<< v.empty() << endl; // checks if the vector is empty, returns 1 if empty, 0 otherwise
+}
+
+int main()
+{
+    ExplainPair();
+    return 0;
+}
