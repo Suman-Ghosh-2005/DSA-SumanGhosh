c0aa4597116f92d753b585f4557f39de48644919
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-06 13:14:49 +0530

    deletion done

---
+++ C++ STL/Vector/deletion_vector.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void ExplainPair()
+{
+    vector<int> v= {1, 2, 3, 4, 5,6};
+    
+   v.erase(v.begin()+1); // deleates the element 2
+
+   v.erase(v.begin()+2,v.begin()+4); // deletes the elements 3 and 4
+    // v.erase(v.begin(),v.end()); // deletes all the elements in the vector
+    // v.clear(); // clears the vector, making it empty
+}
+
+int main()
+{
+    ExplainPair();
+    return 0;
+}
