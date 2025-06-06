fb9a838facc0f57e1c52ac1341cf3c2456470de1
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-06 13:44:16 +0530

    done

---
+++ C++ STL/LIST/LIST_1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Explain_list()
+{
+    list<int>ls;
+    ls.push_back(8);
+    ls.emplace_back(10);
+    ls.push_front(5);
+    ls.emplace_front(3);
+    //rest functions are same as vector
+    // ls.pop_back(); // removes the last element from the list 
+    // ls.pop_front(); // removes the first element from the list
+    // ls.clear(); // clears the list, making it empty
+    // ls.swap(ls1); // swaps the contents of ls and ls1
+
+    // ls.insert(ls.begin(), 5); // inserts 5 at the beginning of the list
+    // ls.insert(ls.begin(), 3, 10); // inserts 3 copies of 10 at the beginning of the list
+    // ls.erase(ls.begin()); // deletes the first element of the list
+    // ls.erase(ls.begin(), ls.end()); // deletes all the elements in the list
+    // ls.sort(); // sorts the elements of the list
+    // ls.reverse(); // reverses the elements of the list
+    // ls.unique(); // removes consecutive duplicate elements from the list
+    
+}
+
+int main()
+{
+
+    return 0;
+}
