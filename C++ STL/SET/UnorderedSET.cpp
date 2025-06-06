9245c4955d24d2a1a877d06c2d4089b3cf2249d6
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-06 15:10:14 +0530

    done

---
+++ C++ STL/SET/UnorderedSET.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Explain_UNset()
+{
+ unordered_set<int> us;
+ // unordered_set is a container that stores unique elements in no particular order.
+// lowerbound and upperbound are not applicable in unordered_set
+
+}
+
+int main()
+{
+    Explain_UNset();
+    return 0;
+}
