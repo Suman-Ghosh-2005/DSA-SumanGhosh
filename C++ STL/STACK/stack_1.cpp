f60a15d50c7eafe760f118345320e8c3d51ea439
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-06 14:03:38 +0530

    done

---
+++ C++ STL/STACK/stack_1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Explain_stack()
+{
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.emplace(30);
+
+    cout << st.top() << endl; // Accessing the top element of the stack which is 30
+    st.pop();                 // Removing the top element of the stack
+    cout << st.top() << endl; // Accessing the new top element of the stack which is 20
+
+    cout << st.size() << endl; // Getting the size of the stack which is 2
+    cout << st.empty() << endl; // Checking if the stack is empty, returns 0 (false) since it has elements
+}
+
+int main()
+{
+
+    return 0;
+}
