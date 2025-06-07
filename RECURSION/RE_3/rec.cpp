b9a202588725d2b36d42e5eac4d22d1b8c37b44c
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-08 00:00:03 +0530

    done

--- RECURSION/RE_3/rec.cpp
+++ RECURSION/RE_3/rec.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// this is the parameterised function to print the sum of first n natural numbers
 void printsum(int n,int sum){
 
     if(n==0){
@@ -13,11 +14,23 @@ void printsum(int n,int sum){
         printsum(n-1,sum+n);
     }
 }
-  
+//now we will do the functional way to print the sum of first n natural numbers
 
+int Print_sum(int n)
+{
+    if(n==1){
+        return 1;
+    }
+    else{
+
+        return n+ Print_sum(n-1);
+    }
+}
 int main()
 {
    int n=4;
-    printsum(n,0);
+   int sum = Print_sum(n); // Call the function to print the sum of first n natural numbers
+
+   cout << sum<< endl;
     return 0;
 }
