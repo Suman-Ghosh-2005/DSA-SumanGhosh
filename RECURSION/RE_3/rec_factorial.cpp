fc3a12c1e1bb18d8670c09d71e398ff0384f468a
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-08 00:08:48 +0530

    done

---
+++ RECURSION/RE_3/rec_factorial.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  print_Factorial(int n){
+if(n==0 || n==1){
+    return 1 ;
+}
+else{
+    return n* print_Factorial(n-1);
+}
+}
+
+int main()
+{
+   int n=4;
+    int factorial = print_Factorial(n); // Call the function to calculate the factorial of n
+    cout << "Factorial of " << n << " is: " << factorial << endl; // Output the result
+
+
+    return 0;
+}
