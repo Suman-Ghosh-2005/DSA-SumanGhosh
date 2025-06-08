7c04d545a96badf8ca5727a349d638b58a1b9da8
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-09 02:20:13 +0530

    Character hashing done

---
+++ HASHING/H_2/Hash_string_1.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Hashing(string s)
+{
+
+    int hash[26] = {0};
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i] - 'a'] += 1;
+    }
+
+    int Query;
+    cout << "enter the number of queries" << endl;
+    cin >> Query;
+
+    while (Query--)
+    {
+        char qE;
+        cout << "enter the element to be searched" << endl;
+        cin >> qE;
+
+        cout << "Count of " << qE << " in the input : " << hash[qE-'a'] << endl; // Print the count of n
+    }
+}
+
+int main()
+{
+
+    string s;
+    cout << "enter the alphabets or any name or words" << endl;
+
+    cin >> s;
+
+    Hashing(s); // Call the Hashing function with the array, max value, and size
+    return 0;
+}
