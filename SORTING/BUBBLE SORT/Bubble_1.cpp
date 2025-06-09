b3c05e94d3214d7810a2cd4e785d849225e363cc
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-09 06:28:40 +0530

    BUBBLE sort done

--- SORTING/BUBBLE SORT/Bubble_1.cpp
+++ SORTING/BUBBLE SORT/Bubble_1.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void BUBBLE_SORT(int arr[], int n)
-{
-  for(int i=n-1;i>0 ; i--){
-    int max_idx = i;
-
-    for(int j=0 ; j<=i ; j++){
-
-        if(arr[j]>arr[max_idx]){
-            max_idx=j;
-        }
-    }
-    swap(arr[i],arr[max_idx]);
-  }
-
-}
 void BuBBLE_SORT(int arr[], int n)
 {
   for(int i=n-1;i>0 ; i--){
