a927daad6ce3140e461b070edaca7b17e9926186
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-09 06:59:54 +0530

    INSERTION sort done

--- SORTING/INSERTION SORT/Insertion_1.cpp
+++ SORTING/INSERTION SORT/Insertion_1.cpp
@@ -3,22 +3,30 @@ using namespace std;
 
 void INSERTION_SORT(int arr[], int n)
 {
-  for(int i=0;i<n-1 ; i++){
-    
-    
 
-    for(int j=0 ; j<=i+1 ; j++){
+    for (int i = 0; i <= n-1; i++)
+    {
+      int is_Sorted=0;
 
-        if(arr[j]>arr[i+1]){
-            swap(arr[j], arr[i+1]); // Swap adjacent elements if they are in the wrong order
-            
+
+        int j = i ;
+
+     
+
+        while (j >0 && arr[j-1] >arr[j])
+        {
+
+            swap(arr[j], arr[j - 1]); // Swap the elements if they are in the wrong order
+            // This effectively shifts the larger element to the right 
+
+           
+
+            j--; // Move to the previous element
         }
+       
 
+     
     }
-  
-    
-  }
-
 }
 
 int main()
@@ -32,9 +40,9 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i]; 
+        cin >> arr[i];
     }
-    INSERTION_SORT(arr, n); 
+    INSERTION_SORT(arr, n);
 
     cout << "Sorted Array is: " << endl;
     for (int i = 0; i < n; i++)
