6d1db453c8ef2e61f7f36a513bffaa68ec825c22
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-14 03:50:08 +0530

    done

--- RECURSION/RE_4/rec_RevArray.cpp
+++ RECURSION/RE_4/rec_RevArray.cpp
@@ -1,38 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+void RevArray(int n[], int size, int i)
+{
 
-void RevArray(int n[],int size,int i){
+    if (i > size / 2)
+    {
 
- if(i>size/2){
+        for (int j = 0; j < size; j++)
+        {
 
-for(int j=0;j<size;j++){
+            cout << n[j] << " ";
+        }
+    }
+    else
+    {
 
-cout << n[j] << " ";
-
+        swap(n[i], n[size - 1 - i]);
+        RevArray(n, size, i + 1); // Recursive call with incremented index
+    }
 }
-
-}
-
-else{
-
-    swap(n[i],n[size-1-i]);
-    RevArray(n, size, i + 1); // Recursive call with incremented index
-
-
-}
- 
-
-}
-  
-
-
 
 int main()
 {
-   int n[5] = {1, 2, 3, 4, 5};
-   int size = sizeof(n)/sizeof(n[0]); // Calculate the size of the array
-    RevArray(n, size,0); // Call the function to reverse the array
+    int n[5] = {1, 2, 3, 4, 5};
+    int size = sizeof(n) / sizeof(n[0]); // Calculate the size of the array
+    RevArray(n, size, 0);                // Call the function to reverse the array
 
     return 0;
 }
