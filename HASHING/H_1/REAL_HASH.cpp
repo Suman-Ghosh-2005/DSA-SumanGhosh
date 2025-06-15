3f42a88eb6ed9408c04e89bd7ce837a6450806c8
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-06-16 00:05:09 +0530

    done

--- HASHING/H_1/REAL_HASH.cpp
+++ HASHING/H_1/REAL_HASH.cpp
@@ -1,53 +1,46 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {    int n;
     int max_val;
 
-
     cout << "Enter the maximum value in the array: ";
-    cin >> max_val; // Input the maximum value in the array
-
+    cin >> max_val; 
 
     cout << "Enter the size of the array: ";
-    cin >> n; // Input the size of the array
-
-
+    cin >> n;
 
     int arr[n];
     cout << "Enter the elements of the array: ";
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i]; // Input the elements of the array
+        cin >> arr[i];
     }
 
-    //precompute
-
-
-    int hash[max_val + 1] = {0};// Create a hash array initialized to 0
+    int hash[max_val + 1] = {0};
 
 
     for (int i = 0; i < n; i++)
   {
-    hash[arr[i]]+=1; // Increment the count for each element in the hash array
+    hash[arr[i]]+=1; 
   }
 
   int queries;
     cout << "Enter the number of queries: ";
-    cin >> queries; // Input the number of queries
+    cin >> queries; 
     while (queries--)
     {
         int q;
         cout << "Enter the element to query: ";
-        cin >> q; // Input the element to query
+        cin >> q;
         if (q < 0 || q > max_val)
         {
-            cout << "Element out of range." << endl; // Check if the queried element is out of range
+            cout << "Element out of range." << endl; 
         }
         else
         {
-            cout << "Count of " << q << " in the array: " << hash[q] << endl; // Output the count of the queried element
+            cout << "Count of " << q << " in the array: " << hash[q] << endl; 
         }
     }
 
