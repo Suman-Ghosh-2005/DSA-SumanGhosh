c9030d3d90e0c1ea81848035ee4ef68d4599bab5
Suman-Ghosh-2005 <sumanofficial20054@gmail.com>
2025-07-10 03:06:10 +0530

    done

--- ARRAY/Moving Zeros to End/arr.cpp
+++ ARRAY/Moving Zeros to End/arr.cpp
@@ -1,8 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void move_Z_End(vector<int> &arr, int n, int k)
+{
+    int j = 0; // Pointer for the next non-zero element
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != k) // If the current element is not zero
+        {
+            arr[j++] = arr[i]; // Place it at the next position
+        }
+    }
+    // Fill the remaining positions with zeros
+    while (j < n)
+    {
+        arr[j++] = k;
+    }
+}
+
 int main()
 {
 
-    return 0;
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter the  elements of array :" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+  
+    move_Z_End(arr, n,k);
+    cout << "After the array is :" << endl;      
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
